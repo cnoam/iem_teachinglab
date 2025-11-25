e991bc310acdb34fc96412ab4a895ced99d40e7b
cnoam <cnoam@technion.ac.il>
2025-11-25 13:42:43 +0200

    Add "forbid creation of personal cluster" policy

--- terraform/dbr/create_objects.tf
+++ terraform/dbr/create_objects.tf
@@ -4,6 +4,8 @@
 resource "databricks_group" "student_groups" {
   for_each     = toset(local.group_names)
   display_name = each.key
+  # Set to false to prevent members of this group from creating unrestricted clusters
+  allow_cluster_create = false
 }
 
 #
@@ -16,4 +18,16 @@ resource "databricks_user" "workspace_user" {
   user_name        = each.key
   workspace_access = false
   active           = true
+}
+
+# -- disable user ability to create personal clusters --
+resource "databricks_cluster_policy" "personal_compute" {
+  name = "Personal Compute"
+  description = "NC use with small-to-medium data or libraries like pandas and scikit-learn. Spark runs in local mode."
+  policy_family_id = "personal-vm"
+  policy_family_definition_overrides = jsonencode(
+    {"node_type_id": {
+      "type": "forbidden" 
+      }
+    })
 }
